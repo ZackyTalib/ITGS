4fa8173c24d8be2293bccddbea9d8d7a83db302f
ZackyTalib <38492137+ZackyTalib@users.noreply.github.com>
2021-10-01 18:11:57 +0800

    final

--- public/category.js
+++ public/category.js
@@ -1,7 +1,25 @@
 const productCategories = {
-    "indobatik": ["http://benzol4.com/wp-content/uploads/2018/11/blog-ph-1.jpg", "http://benzol4.com/wp-content/uploads/2018/11/blog-ph-1.jpg", "http://benzol4.com/wp-content/uploads/2018/11/blog-ph-1.jpg", "http://benzol4.com/wp-content/uploads/2018/11/blog-ph-1.jpg", "http://benzol4.com/wp-content/uploads/2018/11/blog-ph-1.jpg"],
-    "stylebatik": ["http://benzol4.com/wp-content/uploads/2018/11/blog-ph-1.jpg", "http://benzol4.com/wp-content/uploads/2018/11/blog-ph-1.jpg", "http://benzol4.com/wp-content/uploads/2018/11/blog-ph-1.jpg"],
-    "modern": ["http://benzol4.com/wp-content/uploads/2018/11/blog-ph-1.jpg","http://benzol4.com/wp-content/uploads/2018/11/blog-ph-1.jpg", "http://benzol4.com/wp-content/uploads/2018/11/blog-ph-1.jpg", "http://benzol4.com/wp-content/uploads/2018/11/blog-ph-1.jpg", "http://benzol4.com/wp-content/uploads/2018/11/blog-ph-1.jpg", "http://benzol4.com/wp-content/uploads/2018/11/blog-ph-1.jpg"]
+    "indobatik": [
+        "https://mail.google.com/mail/u/1?ui=2&ik=b8ed38c7e2&attid=0.1&permmsgid=msg-f:1712401473602519842&th=17c3ac114aef0322&view=att&disp=safe&realattid=f_ku81njx81",
+        "https://mail.google.com/mail/u/1?ui=2&ik=b8ed38c7e2&attid=0.2&permmsgid=msg-f:1712401473602519842&th=17c3ac114aef0322&view=att&disp=safe&realattid=f_ku81njxv3",
+        "https://mail.google.com/mail/u/1?ui=2&ik=b8ed38c7e2&attid=0.3&permmsgid=msg-f:1712401473602519842&th=17c3ac114aef0322&view=att&disp=safe&realattid=f_ku81njww0",
+        "https://mail.google.com/mail/u/1?ui=2&ik=b8ed38c7e2&attid=0.4&permmsgid=msg-f:1712401473602519842&th=17c3ac114aef0322&view=att&disp=safe&realattid=f_ku81njya4",
+        "https://mail.google.com/mail/u/1?ui=2&ik=b8ed38c7e2&attid=0.5&permmsgid=msg-f:1712401473602519842&th=17c3ac114aef0322&view=att&disp=safe&realattid=f_ku81njxl2",
+        "https://mail.google.com/mail/u/1?ui=2&ik=b8ed38c7e2&attid=0.6&permmsgid=msg-f:1712401473602519842&th=17c3ac114aef0322&view=att&disp=safe&realattid=f_ku81njyp5",
+        "https://mail.google.com/mail/u/1?ui=2&ik=b8ed38c7e2&attid=0.7&permmsgid=msg-f:1712401473602519842&th=17c3ac114aef0322&view=att&disp=safe&realattid=f_ku81njza6"
+    ],
+    "stylebatik": [
+        "https://mail.google.com/mail/u/1?ui=2&ik=b8ed38c7e2&attid=0.1&permmsgid=msg-f:1712401571541601276&th=17c3ac28188ecbfc&view=att&disp=safe&realattid=f_ku81puzt2",
+        "https://mail.google.com/mail/u/1?ui=2&ik=b8ed38c7e2&attid=0.2&permmsgid=msg-f:1712401571541601276&th=17c3ac28188ecbfc&view=att&disp=safe&realattid=f_ku81puzg1",
+        "https://mail.google.com/mail/u/1?ui=2&ik=b8ed38c7e2&attid=0.3&permmsgid=msg-f:1712401571541601276&th=17c3ac28188ecbfc&view=att&disp=safe&realattid=f_ku81pv063",
+        "https://mail.google.com/mail/u/1?ui=2&ik=b8ed38c7e2&attid=0.4&permmsgid=msg-f:1712401571541601276&th=17c3ac28188ecbfc&view=att&disp=safe&realattid=f_ku81puz00"
+    ],
+    "modern": [
+        "https://mail.google.com/mail/u/1?ui=2&ik=b8ed38c7e2&attid=0.1&permmsgid=msg-f:1712401510670264327&th=17c3ac19ec57e807&view=att&disp=safe&realattid=f_ku81opd12",
+        "https://mail.google.com/mail/u/1?ui=2&ik=b8ed38c7e2&attid=0.2&permmsgid=msg-f:1712401510670264327&th=17c3ac19ec57e807&view=att&disp=safe&realattid=f_ku81opdg3",
+        "https://mail.google.com/mail/u/1?ui=2&ik=b8ed38c7e2&attid=0.3&permmsgid=msg-f:1712401510670264327&th=17c3ac19ec57e807&view=att&disp=safe&realattid=f_ku81opc10",
+        "https://mail.google.com/mail/u/1?ui=2&ik=b8ed38c7e2&attid=0.4&permmsgid=msg-f:1712401510670264327&th=17c3ac19ec57e807&view=att&disp=safe&realattid=f_ku81opci1"
+    ]
 }
 
 switch(window.location.href.split("/")[4]){
